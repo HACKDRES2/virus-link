b1d2e10ea14bfbe4652de022e10543294764a3cc
HACKDRES OFICIAL <87175555+HACKDRES2@users.noreply.github.com>
2021-10-26 07:58:51 -0500

    Add files via upload

---
+++ viruslink.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#creador hackdres oficial (mexico)
+
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+
+
+printf "${yellowColour}
+██╗░░░██╗██╗██████╗░██╗░░░██╗░██████╗
+██║░░░██║██║██╔══██╗██║░░░██║██╔════╝
+╚██╗░██╔╝██║██████╔╝██║░░░██║╚█████╗░
+░╚████╔╝░██║██╔══██╗██║░░░██║░╚═══██╗
+░░╚██╔╝░░██║██║░░██║╚██████╔╝██████╔╝
+░░░╚═╝░░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░${endColour}"
+
+echo "creditos a hackdres oficial "
+
+echo "https://youtube.com/channel/UCTSAjBuSJiqSYXd_dqg4qBg "
+
+echo -e "${purpleColour}[01]${endColour}${redColour}.VIRUS${endColour}"
+echo " "
+echo -e "${purpleColour}[02]${endColour}${redColour}.VIRUS${endColour}"
+echo " "
+echo -e "${purpleColour}[03]${endColour}${redColour}.VIRUS${endColour}"
+echo " "
+echo -e "${purpleColour}[04]${endColour}${redColour}.VIRUS${endColour}"
+echo " "
+
+read -p "DIGITE UNA OPCION: " turtle;
+
+echo "  "
+echo -e "${purpleColour}[·]${endColour}${yellowColour} comprobando...${endColour}"
+sleep 5
+echo "  "
+case $turtle in
+
+	1) echo -e "${purpleColour}[-]${endColour}${yellowColour} http://ffmodmenu.page.link/LatestMODMENU${endColour}";;
+
+    2) echo -e "${purpleColour}[-]${endColour}${yellowColour} http://ffmodmenu.page.link/Anti-V${endColour}";;
+
+    3) echo -e "${purpleColour}[-]${endColour}${yellowColour} https://bit.ly/3ild93L${endColour}";;
+
+    4) echo -e "${purpleColour}[-]${endColour}${yellowColour} https://bit.ly/3fX8ljZ${endColour}";;
+
+esac
+
+
+
